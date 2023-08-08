0c8bd2b1339d269ce208a51d014f3102c5ba1d30
Tom Close <tom.g.close@gmail.com>
2023-08-08 22:12:09 +1000

    added bet and zip pipeline specs

---
+++ scripts/test_build.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from click.testing import CliRunner
+from arcana.core.cli.deploy import make_app
+from arcana.core.utils.misc import show_cli_trace
+
+PKG_PATH = Path(__file__).parent.parent.absolute()
+
+runner = CliRunner()
+
+results = runner.invoke(
+    make_app,
+    [
+        f"{PKG_PATH}/australian-imaging-service-community/au/edu/sydney/sydneyimaging/zip.yaml",
+        "xnat:XnatApp",
+        "--raise-errors"
+    ],
+    catch_exceptions=False,
+)
+
+print(show_cli_trace(results))
