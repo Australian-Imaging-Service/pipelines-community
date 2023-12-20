99ed6794960031798dc519801968c0fac631e53b
Tom Close <tom.g.close@gmail.com>
2023-12-20 15:12:22 +1100

    cleaned up yaml spec and added docs test

---
+++ tests/test_docs.py
@@ -0,0 +1,10 @@
+import sys
+from click.testing import CliRunner
+from arcana.core.cli.deploy import make_docs
+
+runner = CliRunner()
+runner.invoke(
+    make_docs,
+    args=sys.argv[1:],
+    catch_exceptions=False
+)
