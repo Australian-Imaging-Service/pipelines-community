8c38b71fbf886899dcc9d16e59fa8ede2bcbeecd
Tom Close <tom.g.close@gmail.com>
2024-08-07 06:47:50 +1000

    fixed up xnat config

--- tutorial/start-up-script.sh
+++ tutorial/start-up-script.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ssh-keygen -t rsa -N "" -f $HOME/.ssh/id_rsa.pub
+echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
 
 # Clone the pipelines-community repository
 mkdir -p ~/git
@@ -13,7 +14,7 @@ pushd ~/git/pipelines-community
 git pull
 
 # Install the pipelines-community repository
-pip install -r ./tutorial/requirements.txt
+pip install --upgrade -r ./tutorial/requirements.txt
 
 # Pre-build/pull the required XNAT docker images to save time
 xnat4tests -c ./tutorial/xnat4tests-config.yaml start --with-data openneuro-t1w
