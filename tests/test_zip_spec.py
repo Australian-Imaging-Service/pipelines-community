5866beb8543b78fc876a76b600a039fc041d3e59
Thomas G. Close <tom.g.close@gmail.com>
2025-08-06 14:36:24 +0930

    updated example specs so that they build with latest pydra2app schema

---
+++ tests/test_zip_spec.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from click.testing import CliRunner
+from pydra2app.core.cli import make
+from frametree.core.utils import show_cli_trace
+
+PKG_PATH = Path(__file__).parent.parent.absolute()
+
+runner = CliRunner()
+
+
+result = runner.invoke(
+    make,
+    [
+        "xnat",
+        "/Users/tclo7153/git/workflows/pipelines-community/specs/australian-imaging-service-community/examples/zip.yaml",
+        "--spec-root",
+        "/Users/tclo7153/git/workflows/pipelines-community/specs",
+        "--for-localhost",
+        "--export-file",
+        "xnat_command.json",
+        "/Users/tclo7153/zip-xnat-command.json",
+    ],
+    catch_exceptions=False,
+)
+
+assert not result.exit_code, show_cli_trace(result)
